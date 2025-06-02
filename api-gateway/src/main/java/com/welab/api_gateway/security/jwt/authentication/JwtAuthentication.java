3073c480d1db57f99cecdaae0b19b3dac0efa28a
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2025-06-02 11:20:25 +0900

    Add files via upload

---
+++ api-gateway/src/main/java/com/welab/api_gateway/security/jwt/authentication/JwtAuthentication.java
@@ -0,0 +1,32 @@
+package com.welab.api_gateway.security.jwt.authentication;
+
+
+import lombok.Getter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Getter
+public class JwtAuthentication extends AbstractAuthenticationToken {
+    private final String token;
+    private final UserPrincipal principal;
+    public JwtAuthentication(UserPrincipal principal, String token, Collection<? extends GrantedAuthority> authorities) {super(authorities);
+        this.principal = principal;
+        this.token = token;
+        this.setDetails(principal);
+        setAuthenticated(true);
+    }
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+    @Override
+    public String getCredentials() {
+        return token;
+    }
+    @Override
+    public UserPrincipal getPrincipal() {
+        return principal;
+    }
+}
